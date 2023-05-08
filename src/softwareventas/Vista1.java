0004a4a101f4d482a8dcf8ceded3d5cff27f78cb
Jhon Gonzalez <jhong2575@gmail.com>
2023-05-08 11:44:38 -0500

    Finalizado

--- src/softwareventas/Vista1.java
+++ src/softwareventas/Vista1.java
@@ -236,7 +236,7 @@ public class Vista1 extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static    void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
